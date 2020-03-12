7e81e3f2b3af19011f03dc99815c93e504757894
TimoThans33 <timotopper@gmail.com>
2020-03-12 01:04:47 +0100

    updates

--- python_files/audio_processing.py
+++ python_files/audio_processing.py
@@ -4,48 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from terminalplot import plot
 import time
+import librosa
+from scipy.fftpack import fft
 
+"""
+rate,audData = scipy.io.wavfile.read("3beeps.wav")
+rate, sampData = scipy.io.wavfile.read("beep_8k_short.wav")
 
-rate,audData=scipy.io.wavfile.read("journey_no_noise_8k.wav")
-rate, sampData = scipy.io.wavfile.read("beep_8k(1).wav")
-
-audtime = np.arange(0, float(audData.shape[0]), 1)
+audtime = np.linspace(0, 1.59, int(audData.shape[0]))
 samptime = np.arange(0, float(sampData.shape[0]), 1)
-
-cross_cor = np.zeros([len(audtime)])
-buffer = np.zeros([len(samptime)])
-delay = np.zeros([len(audtime)])
-hits = []
-print(len(audtime))
-print(len(samptime))
-for i in range(0,len(audtime)-1):
-    timer = time.time()
-    cross_cor[i] = audData[i]*sampData[0]
-    for k in range(len(samptime)-1, 1):
-        cross_cor[i] += buffer[k] * sampData[k]
-        buffer[k] = buffer[k-1]
-    buffer[1] = audData[i]
-    if abs(cross_cor[i] > 0.0015):
-        print("we got a hit!!")
-        hits.append(i)
-    delay[i] = time.time() - timer
-
-print(hits)
-    
-    
-    
 
 plt.figure(0)
 plt.subplot(211)
-
 plt.plot(audtime, audData, linewidth=0.02, alpha=0.9)
-for i in range(0, len(hits)-1):
-    plt.plot(hits[i]+samptime, sampData, linewidth=0.02, alpha=0.9, color='red')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
 
-plt.subplot(212)
-plt.plot(audtime, cross_cor, linewidth=0.02, alpha=0.9)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 
@@ -54,10 +26,70 @@ plt.subplot(211)
 plt.plot(samptime, sampData, linewidth=0.1, alpha=0.9)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
+"""
 
+audData, rate = librosa.core.load("3beeps.wav", sr = None)
+sampData, rate = librosa.core.load("beep_8k_short.wav", sr = None)
+
+audtime = np.arange(0, len(audData), 1)
+samptime = np.arange(0, len(sampData), 1)
+
+"""
+plt.figure(0)
 plt.subplot(212)
-plt.plot(audtime, delay, linewidth=0.1, alpha=0.9)
+plt.plot(audtime, audData, linewidth=0.02, alpha=0.9)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+plt.figure(1)
+plt.subplot(212)
+plt.plot(samptime, sampData, linewidth=0.5, alpha=0.9)
 plt.xlabel('Time (s)')
 plt.ylabel('delay')
-plt.ylim(0, 5*10**-5)
 plt.show()
+"""
+
+cross_cor = np.zeros([len(audtime)])
+buffer = np.zeros([len(samptime)])
+delay = np.zeros([len(audtime)])
+hits = []
+N = len(samptime)-1
+
+print(len(audData))
+print(len(sampData))
+
+for i in range(0,len(audtime)-1):
+    timer = time.time()
+    cross_cor[i] = audData[i]*sampData[0]
+    for k in range(0, len(samptime)-1):
+        cross_cor[i] += buffer[k] * sampData[k]
+        buffer[N-k] = buffer[(N-1)-k]
+    buffer[0] = audData[i]
+    if abs(cross_cor[i] > 0.3):
+        print("we got a hit!!")
+        print("cross correlation =", cross_cor[i])
+        hits.append(i)
+    delay[i] = time.time() - timer
+
+print(hits)
+
+
+
+
+plt.figure(0)
+plt.plot(audtime, audData, linewidth=0.05, alpha=0.08, label='input signal')
+for i in range(0, len(hits)-1):
+    plt.bar(hits[i], height = 0.4, width = 50, bottom=-0.2, alpha=0.1,
+     color='red', label='hits')
+
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.xticks()
+plt.yticks()
+
+plt.figure(1)
+plt.plot(audtime, cross_cor, linewidth=0.02, alpha=0.9, label='beep sample')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.xticks()
+plt.yticks()
--- python_files/fft_croscor.py
+++ python_files/fft_croscor.py
@@ -4,79 +4,80 @@ import numpy as np
 import matplotlib.pyplot as plt
 from terminalplot import plot
 import time
+import librosa
 from scipy.fftpack import fft
 
 
-rate,audData=scipy.io.wavfile.read("journey_no_noise_8k.wav")
-rate, sampData = scipy.io.wavfile.read("beep_8k(1).wav")
+audData, rate = librosa.core.load("3beeps.wav", sr=None)
+sampData, rate = librosa.core.load("beep_8k_short.wav", sr=None)
 
-audtime = np.arange(0, float(audData.shape[0]), 1)
-samptime = np.arange(0, float(sampData.shape[0]), 1)
+audtime = np.arange(0, len(audData), 1)
+samptime = np.arange(0, len(sampData), 1)
 
-frequency_samp = np.linspace(0.0, 512, int(len(samptime)))
-frequency_aud = np.linspace(0.0, 512, int(len(audtime)))
+frequency_samp = np.linspace(0.0, 512, int(len(samptime)/2))
+frequency_aud = np.linspace(0.0, 512, int(len(audtime)/2))
 
 freq_data_samp = fft(sampData)
 freq_data_aud = fft(audData)
 
-X_sample = 2/len(samptime)*np.abs(freq_data_samp[0:np.int(len(samptime))])
-X_audio = 2/len(audtime)*np.abs(freq_data_aud[0:np.int(len(audtime))])
+X_sample = 2/len(samptime)*np.abs(freq_data_samp[0:np.int(len(samptime)/2)])
+X_audio = 2/len(audtime)*np.abs(freq_data_aud[0:np.int(len(audtime)/2)])
+
+plt.figure(0)
+plt.subplot(211)
+plt.plot(samptime, sampData, linewidth=0.05, alpha=0.9, label='beep_sample')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.legend()
+
+plt.subplot(212)
+plt.plot(frequency_samp, X_sample, linewidth=0.1, alpha=1, label='FFT')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.legend()
+
+plt.show()
 
 cross_cor = np.zeros([len(audtime)])
-buffer = np.zeros(20)
+buffer = np.zeros(len(X_sample))
+Xjw_buf = np.zeros(len(X_sample))
 delay = np.zeros([len(audtime)])
 DFT = np.zeros([len(audtime)])
 
+sum = 0
+counter = 0
 hits = []
+N = len(X_sample)-1
 
 print(len(audtime))
-print(len(samptime))
-for i in range(0,len(audtime)-1):
+print(len(X_sample))
+
+for i in range(0,int(len(audtime))-1):
     timer = time.time()
-    cross_cor[i] = X_audio[i]*X_sample[0]
-    for k in range(len(buffer)-1, 1):
-        cross_cor[i] +=  Xjw_buf[k]* X_sample[k]
-        buffer[k] = buffer[k-1]
-    buffer[1] = audData[i]
-    Xjw_buf = 2/len(buffer)*np.abs(fft(buffer)[0:len(buffer)])
-    if abs(cross_cor[i] > 0.0019):
+    sum =  0
+    #cross_cor_t[i] = audData[i]*sampData[0]
+    #Xjw_buf = 2/len(buffer)*np.abs(fft(buffer)[0:len(buffer)/2])
+    for k in range(0, len(X_sample)-1):
+        sum += np.abs(buffer[N-k]*np.exp(-2j*np.pi/len(Xjw_buf)*(N-k)*counter))
+        cross_cor[i] +=  Xjw_buf[N-k] * X_sample[N-k]
+        #cross_cor_t[N-k] += buffer[N-k] * sampData[N-k]
+        buffer[N-k] = buffer[(N-1)-k]
+        Xjw_buf[N-k] = Xjw_buf[(N-1)-k]
+    buffer[0] = audData[i]
+    Xjw_buf[0] = sum
+    print(Xjw_buf[len(X_sample)-1])
+    if counter >= len(X_sample):
+        counter = 0
+    if abs(cross_cor[i]) > 100000000000:
         print("we got a hit!!")
         hits.append(i)
+    counter += 1
     delay[i] = time.time() - timer
 
 print(hits)
 
-
-
 plt.figure(0)
-plt.subplot(211)
-
-plt.plot(audtime, X_audio, linewidth=0.02, alpha=0.9)
-"""
-for i in range(0, len(hits)-1):
-    plt.plot(hits[i]+samptime, sampData, linewidth=0.02, alpha=0.9, color='red')
-    """
+plt.plot(audtime, cross_cor, linewidth=0.1, alpha=0.9)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
-
-plt.subplot(212)
-plt.plot(audtime, cross_cor, linewidth=0.5, alpha=0.9)
-"""
-for i in range(0, len(hits)-1):
-    plt.plot(hits[i]+samptime, sampData, linewidth=0.01, alpha=0.9, color='red')
-"""
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-
-plt.figure(1)
-plt.subplot(211)
-plt.plot(frequency_samp, X_sample, linewidth=0.1, alpha=0.9)
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-
-plt.subplot(212)
-plt.plot(audtime, delay, linewidth=0.1, alpha=0.9)
-plt.xlabel('Time (s)')
-plt.ylabel('delay')
-plt.ylim(0, 5*10**-5)
 plt.show()
